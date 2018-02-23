1c399179061b02a2282c1fcf8c9d8219924efcf4
Leo Liu <LY6819@HOTMAIL.COM>
2018-02-23 15:59:57 +0800

    Update util.js

--- util.js
+++ util.js
@@ -234,3 +234,34 @@ const _random = function(count, maxSize) {
     }
     return Array.from(mySet);
 };
+
+class Shape {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
+  get area() {
+    return this._width * this._height;
+  }
+}
+
+const handler = {
+  get: function(target, key) {
+    if (key[0] === '_') {
+      throw new Error('Attempt to access private property');
+    }
+    return target[key];
+  },
+  set: function(target, key, value) {
+    if (key[0] === '_') {
+      throw new Error('Attempt to access private property');
+    }
+    target[key] = value;
+  }
+}
+
+const square = new Proxy(new Shape(10, 10), handler);
+console.log(square.area);             // 100
+console.log(square instanceof Shape); // true
+square._width = 200;  
+https://juejin.im/post/5a8e9b6d5188257a5f1ed826
